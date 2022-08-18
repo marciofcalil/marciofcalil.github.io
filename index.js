4c33e4924118efe815133156726df78915d46e13
Márcio Fernandes Calil <marciofcalil@gmail.com>
2022-08-18 19:13:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 const tokenString = document.getElementById("token");
 const errorMessage = document.getElementById("error");
 const message = document.getElementById("message");
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZw0enmJRnC5lOd0KgHARt7zyLnGiW5oc",
+    authDomain: "testepushpwamfc.firebaseapp.com",
+    projectId: "testepushpwamfc",
+    storageBucket: "testepushpwamfc.appspot.com",
+    messagingSenderId: "539926553453",
+    appId: "1:539926553453:web:500c5818d625a84030f064",
+    measurementId: "G-SMSQ3Z8T1Z"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDZw0enmJRnC5lOd0KgHARt7zyLnGiW5oc",
+    authDomain: "testepushpwamfc.firebaseapp.com",
+    projectId: "testepushpwamfc",
+    storageBucket: "testepushpwamfc.appspot.com",
+    messagingSenderId: "539926553453",
+    appId: "1:539926553453:web:500c5818d625a84030f064",
+    measurementId: "G-SMSQ3Z8T1Z"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
